f390dabd4d39635a3b2994f0c9963db0af018f83
goldo63 <t.aerts@hotmail.com>
2023-11-08 15:53:20 +0100

    routing

--- apps/dreams/src/app/app.routes.ts
+++ apps/dreams/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import { AboutComponent } from './Pages/About/about.component'
+import { LayoutComponent } from './Templates/layout/layout.component'
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+          { path: '', pathMatch: 'full', component: AboutComponent },
+          { path: 'home', pathMatch: 'full', redirectTo: '' },
+          { path: 'about', pathMatch: 'full', component: AboutComponent },
+        ]
+    }
+];
