bdfc7346b06e32bff0becfc9b04fe8aaef48f5e4
goldo63 <t.aerts@hotmail.com>
2023-11-30 19:40:29 +0100

    Cleaned out api

--- apps/data-api/src/app/app.controller.ts
+++ apps/data-api/src/app/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return {
+      message: 'Hello API',
+      endpoints: [
+        { post: ['/', '/create', '/:tag', '/:id', '/:id/update', '/:id/delete'] },
+        { user: ['/', '/create', '/:id', '/:id/update', '/:id/delete'] },
+        { reaction: ['/', '/create', '/:id', '/:id/update', '/:id/delete'] }
+
+      ]
+    };
   }
 }
