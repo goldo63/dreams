ea4aea92ed6f9b30f8431f7b93cc58c701bbcadc
goldo63 <t.aerts@hotmail.com>
2023-12-05 23:21:06 +0100

    post api connected with db

--- libs/backend/features/src/lib/post/post.controller.ts
+++ libs/backend/features/src/lib/post/post.controller.ts
@@ -1,20 +1,39 @@
-import { Controller, Get, Logger, Param } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  NotFoundException,
+  Param,
+  Res,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { IPost } from '@dreams/shared/models';
 
 @Controller('post')
 export class PostController {
-    private readonly logger = new Logger(PostController.name);
+  private readonly logger = new Logger(PostController.name);
 
-    constructor(private postService: PostService) {}
+  constructor(private postService: PostService) {}
 
-    @Get('')
-    getAll(): Promise<IPost[]> {
-        return this.postService.getAllpublic();
-    }
+  @Get('')
+  async getAll(): Promise<IPost[]> {
+    return this.postService.getAllpublic();
+  }
 
-    @Get(':id')
-    getById(@Param('id') id: number): Promise<IPost> {
-        return this.postService.getById(id);
-    }
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<IPost | null> {
+    const result = this.postService.getById(id);
+
+    if (!(await result)) throw new NotFoundException(`Post with id ${id} not found`);
+    return result;
+  }
+
+  @Delete(':id')
+  async deletePostById(@Param('id') id: number, @Res() res: Response): Promise<void> {
+    const result = await this.postService.deleteById(id);
+
+    if (result.deletedCount === 0) throw new NotFoundException(`Post with ID ${id} not found`);
+    (res as any).status(200).json({ message: 'Post deleted successfully' });
+  }
 }
--- libs/backend/features/src/lib/post/post.service.ts
+++ libs/backend/features/src/lib/post/post.service.ts
@@ -1,22 +1,32 @@
-import { Injectable, Logger, NotFoundException } from "@nestjs/common";
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { IPost, ReadAbility } from '@dreams/shared/models';
-import { BehaviorSubject, Observable, catchError, delay, filter, from, map, of, take } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  delay,
+  filter,
+  from,
+  map,
+  of,
+  take,
+} from 'rxjs';
 import { Post as PostModel, PostDocument } from './post.schema';
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PostService {
-    private readonly logger: Logger = new Logger(PostService.name);
-    private posts: IPost[] = [
-      {
-        id: 0,
-        posterId: 1,
-        postDate: new Date(),
-        title: 'Help with Sustainable Agriculture Project',
-        imgUrl: 'https://example.com/agriculture.jpg',
-        videoUrl: 'https://www.youtube.com/watch?v=example',
-        content: `
+  private readonly logger: Logger = new Logger(PostService.name);
+  private posts: IPost[] = [
+    {
+      id: 0,
+      posterId: 1,
+      postDate: new Date(),
+      title: 'Help with Sustainable Agriculture Project',
+      imgUrl: 'https://example.com/agriculture.jpg',
+      videoUrl: 'https://www.youtube.com/watch?v=example',
+      content: `
           <p>Hello dreamers! 👋</p>
           <p>I'm working on a sustainable agriculture project and could use some help.</p>
           <p>Key goals include:</p>
@@ -28,153 +38,131 @@ export class PostService {
           <p>If you have expertise in agriculture or sustainability, I'd love to connect!</p>
           <img src="https://example.com/agriculture-field.jpg" alt="Agriculture Field">
         `,
-        readAbility: ReadAbility.public,
-      },
-      {
-        id: 1,
-        posterId: 2,
-        postDate: new Date(),
-        title: 'Community Art Mural Project',
-        imgUrl: 'https://example.com/art-mural.jpg',
-        videoUrl: '',
-        content: `
+      readAbility: ReadAbility.public,
+    },
+    {
+      id: 1,
+      posterId: 2,
+      postDate: new Date(),
+      title: 'Community Art Mural Project',
+      imgUrl: 'https://example.com/art-mural.jpg',
+      videoUrl: '',
+      content: `
           <p>Hi everyone! 🎨</p>
           <p>I'm organizing a community art mural project in our neighborhood.</p>
           <p>Let's come together to create something beautiful that reflects the spirit of our community.</p>
           <p>No art experience needed – just enthusiasm and a love for creativity!</p>
           <img src="https://example.com/community-mural.jpg" alt="Community Art Mural">
         `,
-        readAbility: ReadAbility.public,
-      },
-      {
-        id: 2,
-        posterId: 3,
-        postDate: new Date(),
-        title: 'Tech Enthusiasts Wanted for Coding Project',
-        imgUrl: '',
-        videoUrl: 'https://www.youtube.com/watch?v=example',
-        content: `
+      readAbility: ReadAbility.public,
+    },
+    {
+      id: 2,
+      posterId: 3,
+      postDate: new Date(),
+      title: 'Tech Enthusiasts Wanted for Coding Project',
+      imgUrl: '',
+      videoUrl: 'https://www.youtube.com/watch?v=example',
+      content: `
           <p>Hey coders! 💻</p>
           <p>I'm working on an open-source coding project and need collaborators.</p>
           <p>Our goal is to develop a useful tool for developers around the world.</p>
           <p>If you're passionate about coding and want to contribute, let's connect!</p>
         `,
-        readAbility: ReadAbility.public,
-      },
-      {
-        id: 3,
-        posterId: 4,
-        postDate: new Date(),
-        title: 'Fitness Challenge: Join Me on the Journey!',
-        imgUrl: 'https://example.com/fitness-challenge.jpg',
-        videoUrl: '',
-        content: `
+      readAbility: ReadAbility.public,
+    },
+    {
+      id: 3,
+      posterId: 4,
+      postDate: new Date(),
+      title: 'Fitness Challenge: Join Me on the Journey!',
+      imgUrl: 'https://example.com/fitness-challenge.jpg',
+      videoUrl: '',
+      content: `
           <p>Hey fitness enthusiasts! 💪</p>
           <p>I'm embarking on a fitness challenge to improve my health and well-being.</p>
           <p>Join me on this journey, share your fitness tips, and let's motivate each other!</p>
           <img src="https://example.com/fitness-journey.jpg" alt="Fitness Challenge">
         `,
-        readAbility: ReadAbility.public,
-      },
-      {
-        id: 4,
-        posterId: 5,
-        postDate: new Date(),
-        title: 'Book Club: Exploring New Literary Worlds',
-        imgUrl: 'https://example.com/book-club.jpg',
-        videoUrl: '',
-        content: `
+      readAbility: ReadAbility.public,
+    },
+    {
+      id: 4,
+      posterId: 5,
+      postDate: new Date(),
+      title: 'Book Club: Exploring New Literary Worlds',
+      imgUrl: 'https://example.com/book-club.jpg',
+      videoUrl: '',
+      content: `
           <p>Hello bookworms! 📚</p>
           <p>I'm starting a book club to explore new literary worlds and engage in meaningful discussions.</p>
           <p>If you're passionate about reading, join us for this exciting literary adventure!</p>
           <img src="https://example.com/book-club-meeting.jpg" alt="Book Club Meeting">
         `,
-        readAbility: ReadAbility.public,
-      },
-      {
-        id: 5,
-        posterId: 6,
-        postDate: new Date(),
-        title: 'Cooking Class: Discover the Art of Culinary Delights',
-        imgUrl: 'https://example.com/cooking-class.jpg',
-        videoUrl: '',
-        content: `
+      readAbility: ReadAbility.public,
+    },
+    {
+      id: 5,
+      posterId: 6,
+      postDate: new Date(),
+      title: 'Cooking Class: Discover the Art of Culinary Delights',
+      imgUrl: 'https://example.com/cooking-class.jpg',
+      videoUrl: '',
+      content: `
           <p>Foodies unite! 🍲</p>
           <p>I'm hosting a cooking class to explore the art of culinary delights.</p>
-          <p>Let's share recipes, cooking tips, and create delicious meals together!</p>
+          <p>Let's share recipes, cooking tips, and create delicious posts together!</p>
           <img src="https://example.com/cooking-class-kitchen.jpg" alt="Cooking Class Kitchen">
         `,
-        readAbility: ReadAbility.public,
-      },
-      ];
+      readAbility: ReadAbility.public,
+    },
+  ];
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    constructor(
-      @InjectModel(PostModel.name) private postModel: Model<PostDocument>,
-    ) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor(
+    @InjectModel(PostModel.name) private postModel: Model<PostDocument>
+  ) {}
 
-    async getAllpublic(): Promise<IPost[]> {
-      this.logger.log(`Finding all items`);
-      const items = await this.postModel
-          .find()
-          .exec();
-      return items;
+  async getAllpublic(): Promise<IPost[]> {
+    this.logger.log(`Finding all public posts`);
+
+    const items = await this.postModel
+      .find({ readAbility: 2 }) // 2 = public
+      .exec();
+    return items;
+  }
+
+  async getById(id: number): Promise<IPost | null> {
+    this.logger.log(`Finding post by id ${id}`);
+
+    const item = await this.postModel.findOne({ id: +id }).exec();
+
+    if (!item) return null;
+    return item;
+  }
+
+  async create(req: any): Promise<IPost | null> {
+    const post = req.body;
+    const user_id = req.user.user_id;
+
+    if (post && user_id) {
+      this.logger.log(`Create post ${post.title}`);
+      return this.postModel.create(post);
     }
+    return null;
+  }
 
-    // async getAllpublic(): Promise<IPost[]> {
-    //     return this.posts.filter(post => post.readAbility === ReadAbility.public);
-    // }
+  async update(_id: string, post: IPost): Promise<IPost | null> {
+    this.logger.log(`Update post ${post.title}`);
+    return this.postModel.findByIdAndUpdate({ _id }, post);
+  }
 
-    async getById(id: number): Promise<IPost> {
-      const parsedId = parseInt(id.toString(), 10); // Ensure the ID is a number
-      const foundPost = this.posts.find(post => post.id === parsedId);
-    
-      if (!foundPost) {
-        throw new NotFoundException(`Post with ID ${parsedId} not found`);
-      }
-    
-      return foundPost;
-    }
+  async deleteById(id: number): Promise<{ deletedCount: number }> {
+    this.logger.log(`Deleting post by ID: ${id}`);
+    const result = await this.postModel
+      .deleteOne({ id: id })
+      .exec();
 
-    create(post: IPost): Observable<IPost[]> {
-        console.log('creating post');
-        return of(post).pipe(
-          map((newPost: IPost) => {
-            this.posts.push(newPost);
-            return this.posts;
-          })
-        );
-    }
-
-    update(post: IPost): Observable<IPost[]> {
-        console.log('updating post');
-        return of(post).pipe(
-          map((updatedpost: IPost) => {
-            const index = this.posts.findIndex(u => u.id === updatedpost.id);
-            if (index !== -1) {
-              this.posts[index] = updatedpost;
-            }
-      
-            // Return the updated array
-            return this.posts;
-          })
-        );
-    }
-
-    delete(id: number): Observable<IPost[]> {
-        return of(id).pipe(
-          map((deleteId: number) => {
-            const index = this.posts.findIndex(post => post.id === deleteId);
-      
-            if (index !== -1) {
-              this.posts.splice(index, 1);
-            }
-            else {
-                console.log(`post with id ${deleteId} not found.`);
-            }
-      
-            return this.posts;
-          }),
-        );
-      }
+    return result;
+  }
 }
