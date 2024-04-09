b72e9e101580d3b9e9713ae9b8a0cc8289b23771
goldo63 <t.aerts@hotmail.com>
2024-04-09 07:42:57 +0200

    Added pakcage.json to dist

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -1,8 +1,17 @@
 const { composePlugins, withNx } = require('@nx/webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-// Nx plugins for webpack.
-module.exports = composePlugins(withNx(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
-  return config;
-});
+module.exports = composePlugins(
+  withNx(),
+  (config) => {
+    // Add CopyWebpackPlugin to copy the root package.json to dist
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: '../../package.json', to: 'package.json' } // Adjust the paths if needed
+        ]
+      })
+    );
+    return config;
+  }
+);
